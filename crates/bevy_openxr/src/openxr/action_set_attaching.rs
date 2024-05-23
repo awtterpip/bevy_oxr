99a52dac1e56ca1fb289c4cc426f527938399ab4
Jay Christy <fortehlose@gmail.com>
2024-05-23 14:03:23 -0400

    formatting

--- crates/bevy_openxr/src/openxr/action_set_attaching.rs
+++ crates/bevy_openxr/src/openxr/action_set_attaching.rs
@@ -14,14 +14,11 @@ impl Plugin for OxrActionAttachingPlugin {
 
 fn attach_sets(session: Res<OxrSession>, mut events: EventReader<OxrAttachActionSet>) {
     let sets = events.read().map(|v| &v.0).collect::<Vec<_>>();
-    if sets.is_empty() {
-        return;
-    }
+    if sets.is_empty() {return;}
     info!("attaching {} sessions", sets.len());
     match session.attach_action_sets(&sets) {
-        Ok(_) => {
-            info!("attached sessions!");
-        }
+        Ok(_) => {info!("attached sessions!");
+    }
         Err(openxr::sys::Result::ERROR_ACTIONSETS_ALREADY_ATTACHED) => {
             error!("Action Sets Already attached!");
         }
