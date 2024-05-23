870cb57a9924fb1ad889c949dda81f00fff403d4
Jay Christy <fortehlose@gmail.com>
2024-05-23 14:02:20 -0400

    removed uneeded changes

--- crates/bevy_openxr/src/openxr/action_set_attaching.rs
+++ crates/bevy_openxr/src/openxr/action_set_attaching.rs
@@ -1,7 +1,6 @@
 use crate::resources::OxrSession;
 use bevy::prelude::*;
 use bevy_xr::session::status_changed_to;
-use openxr::ActionSet;
 
 impl Plugin for OxrActionAttachingPlugin {
     fn build(&self, app: &mut App) {
@@ -10,15 +9,10 @@ impl Plugin for OxrActionAttachingPlugin {
             PostUpdate,
             attach_sets.run_if(status_changed_to(bevy_xr::session::XrStatus::Ready)),
         );
-        app.init_resource::<AttachedActionSets>();
     }
 }
 
-fn attach_sets(
-    session: Res<OxrSession>,
-    mut events: EventReader<OxrAttachActionSet>,
-    mut attached: ResMut<AttachedActionSets>,
-) {
+fn attach_sets(session: Res<OxrSession>, mut events: EventReader<OxrAttachActionSet>) {
     let sets = events.read().map(|v| &v.0).collect::<Vec<_>>();
     if sets.is_empty() {
         return;
@@ -27,10 +21,6 @@ fn attach_sets(
     match session.attach_action_sets(&sets) {
         Ok(_) => {
             info!("attached sessions!");
-            for &set in sets.iter() {
-                let clone = set.clone();
-                attached.sets.push(clone);
-            }
         }
         Err(openxr::sys::Result::ERROR_ACTIONSETS_ALREADY_ATTACHED) => {
             error!("Action Sets Already attached!");
@@ -50,8 +40,3 @@ fn attach_sets(
 pub struct OxrAttachActionSet(pub openxr::ActionSet);
 
 pub struct OxrActionAttachingPlugin;
-
-#[derive(Resource, Default)]
-pub struct AttachedActionSets {
-    pub sets: Vec<ActionSet>,
-}
